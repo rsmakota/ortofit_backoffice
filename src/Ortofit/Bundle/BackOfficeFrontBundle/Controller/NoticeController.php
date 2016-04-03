75b7de8660847e19aee40a2cb0a3374f5b3b6041
Rodion Smakota <rsmakota@gmail.com>
2016-04-03 23:12:39 +0300

    tyr

---
+++ src/Ortofit/Bundle/BackOfficeFrontBundle/Controller/NoticeController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright 2016 ortofit_backoffice
+ * @author Rodion Smakota <rsmakota@gmail.com>
+ */
+
+namespace Ortofit\Bundle\BackOfficeFrontBundle\Controller;
+
+/**
+ * Class NoticeController
+ * @package Ortofit\Bundle\BackOfficeFrontBundle\Controller
+ */
+class NoticeController extends BaseController
+{
+
+    public function indexAction()
+    {
+
+    }
+}
